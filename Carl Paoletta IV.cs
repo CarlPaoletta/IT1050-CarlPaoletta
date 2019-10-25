b74f1b8134368a33406b49da9109682db0930a2e
CarlPaoletta <56942165+CarlPaoletta@users.noreply.github.com>
2019-10-25 13:17:24 -0400

    Add files via upload

---
+++ Carl Paoletta IV.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Week_One_Carl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This class is awesome
+
+            string str1, str2;
+            str1 = "hello world";
+            str2 = "Carl Paoletta IV";
+            int num1, num2, num3;
+
+            num1 = 27;
+            num2 = 55;
+
+            num3 = num1 + num2;
+
+            Console.WriteLine(" str1 + str2 ");
+            Console.WriteLine("hello");
+        }
+    }
+}
